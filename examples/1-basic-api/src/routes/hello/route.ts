acce3ad8f9152fc8d39c8e9ea2d56dc70af6d218
Lucas Larangeira <vivaz.luke@icloud.com>
2025-09-29 20:14:42 -0300

    refactor: remove delay na rota hello
    - Remove a promessa de 5 segundos na rota hello, melhorando a resposta da API.

--- examples/1-basic-api/src/routes/hello/route.ts
+++ examples/1-basic-api/src/routes/hello/route.ts
@@ -1,6 +1,5 @@
 import type { LithiaRequest, LithiaResponse } from 'lithia';
 
 export default async (_: LithiaRequest, res: LithiaResponse) => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   res.send('Hello, from Lithia! 🚀');
 };
